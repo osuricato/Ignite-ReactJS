7d6ab430cb2540e9f92931bcd614c19284db68b6
samuel.gomes <samuel.gomes@precato.com.br>
2022-02-21 18:46:21 -0300

    creating api fake

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,14 +1,12 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
-import { TransactionsTable } from "./components/TransactionsTable";
 
 export function App() {
   return (
     <>
       <Header />
       <Dashboard />
-      <TransactionsTable />
       <GlobalStyle />
     </>
   );
