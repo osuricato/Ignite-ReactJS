ad5874f3346a4298224c78a90a6b37cda31ad848
samuel.gomes <samuel.gomes@precato.com.br>
2022-03-08 13:50:18 -0300

    subscribe user

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { useSession, signIn } from 'next-auth/react';
+
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe.js';
+
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,8 +10,35 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const session  = useSession()
+  
+  async function handleSubcribe() {
+    if(!session) {
+      signIn('github')
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({
+        sessionId
+      })
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubcribe}
+    >
       Subscribe now
     </button>
   )
