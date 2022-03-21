db8829b1e622442402b72ae8428d9ca69cf82cd8
samuel.gomes <samuel.gomes@precato.com.br>
2022-03-21 10:28:45 -0300

    login page

---
+++ dashgo/src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import Document, { Html } from 'next/document'
+import Head from 'next/head'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" /> 
+        </Head>
+      </Html>
+    )
+  }
+}
--- dashgo/src/pages/index.tsx
+++ dashgo/src/pages/index.tsx
@@ -1,5 +1,36 @@
+import { Flex, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
+
 export default function Home() {
   return (
-    <h1>Dashboard</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input type="email" name="email" label="Email"/>
+          <Input type="password" name="password" label="Senha"/>
+        </Stack>
+        
+        <Button
+          mt="6"
+          type="submit"
+          colorScheme="pink"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
