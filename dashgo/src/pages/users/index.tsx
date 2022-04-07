b2ef03be57ce09eb9d6c4169309e2ad2da02eec2
samuel.gomes <samuel.gomes@precato.com.br>
2022-04-07 08:56:18 -0300

    creating list hook and pagination logical

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -1,34 +1,14 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from 'react-query'
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const responde = await fetch('http://localhost:3000/api/users')
-    const data = await responde.json()
-
-    // const users = data.users.map(user => {
-    //   return {
-    //     id: user.id,
-    //     name: user.name,
-    //     email: user.email,
-    //     createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-    //       day: '2-digit',
-    //       month: 'long',
-    //       year: 'numeric'
-    //     })
-    //   }
-    // })
-
-    return data;
-  }, {
-    staleTime: 1000 * 5
-  });
+  const { data, isLoading, isFetching, error } = useUsers()
 
   const isWideVersion = useBreakpointValue({
     base: true,
