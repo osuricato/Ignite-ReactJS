ebb674ddd4f0f196793e571c69c7b11c2b0bceca
Samuel Souza <samuel.souzagomes001@gmail.com>
2022-04-15 20:15:57 -0300

    send bearer token

--- nextauth/contexts/AuthContext.tsx
+++ nextauth/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
-import { setCookie } from 'nookies'
-import { createContext, ReactNode, useState } from "react";
+import { setCookie, parseCookies } from 'nookies'
+import { createContext, ReactNode, useEffect, useState } from "react";
 
 import { api } from "../services/api";
 
@@ -33,6 +33,22 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>({} as User);
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies()
+
+    if (token) {
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data
+
+        setUser({
+          email,
+          permissions,
+          roles
+        })
+      })
+    }
+  }, [])
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post('/sessions', { email, password });
@@ -54,6 +70,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
         permissions,
         roles
       })
+
+      api.defaults.headers.common.Authorization = `Bearer ${token}`
 
       router.push('/dashboard')
     } catch (error) {
