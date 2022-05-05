e8f4df87c875cd411f40c77a171f261c1a4a0a5d
samuel.gomes <samuel.gomes@precato.com.br>
2022-05-05 08:58:04 -0300

    finish perform

--- perform-app/components/ProductItem.tsx
+++ perform-app/components/ProductItem.tsx
@@ -1,19 +1,45 @@
-import { memo } from 'react'
+import { memo, useState, lazy } from 'react'
+import { AddProductToWishListProps } from './AddProductToWishList'
+import dynamic from 'next/dynamic'
+
+// import { AddProductToWishList } from './AddProductToWishList';
+
+const AddProductToWishList = dynamic<AddProductToWishListProps>(() => {
+  return import('./AddProductToWishList').then((mod) => mod.AddProductToWishList)
+}, {
+  loading: () => <span>Carregando...</span>
+})
 
 interface ProductItemProps {
   product: {
     id: number,
-    price: number,
+    priceFormatted: string,
     title: string;
   }
   onAddToWishList: (id: number) => void
 }
 
 function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
+  const [isAddToWishList, setIsAddToWishList] = useState(false)
+
+  async function showFormattedDate() {
+    const { format } = await import('date-fns')
+
+    // format()
+  }
+
   return (
     <div>
-      {product.title} - <strong>{product.price}</strong>
-      <button onClick={() => onAddToWishList(product.id)}> to wish list</button>
+      {product.title} - <strong>{product.priceFormatted}</strong>
+
+      <button onClick={() => setIsAddToWishList(true)}>Adicionar aos favoritos</button>
+
+      { isAddToWishList && (
+        <AddProductToWishList 
+          onAddToWishList={() => onAddToWishList(product.id)}
+          onRequestClose={() => setIsAddToWishList(false)}
+        />
+      )}
     </div>
   )
 }
--- perform-app/components/SearchResults.tsx
+++ perform-app/components/SearchResults.tsx
@@ -1,32 +1,49 @@
-import { useMemo } from 'react'
+import { List, ListRowRenderer } from 'react-virtualized'
 
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
+  totalPrice: number;
   results: Array<{
     id: number,
-    price: number,
+    priceFormatted: string,
     title: string;
   }>
   onAddToWishList: (id: number) => void
 }
 
-export function SearchResults({ results, onAddToWishList }: SearchResultsProps) {
-  const totalPrice = useMemo(() => {
-    results.reduce((total, product) => {
-      return total + product.price;
-    }, 0)
-  }, [results])
+export function SearchResults({ results, totalPrice, onAddToWishList }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style } => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    )
+  })
 
   return (
     <div>
-      {results && results.map(product => (
+      <h2>{totalPrice}</h2>
+
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+
+      {/* {results && results.map(product => (
         <ProductItem
           key={product.id}  
           product={product}
           onAddToWishList={onAddToWishList}
         />
-      ))}
+      ))} */}
     </div>
   )
 }
